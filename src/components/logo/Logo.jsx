3ded7dfa6f6537f8acb802f93d5831fdb89ffe08
Momo M. <momomarcoj@gmail.com>
2024-08-18 00:47:23 +0200

    Updated logo margin bottom

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,7 +1,7 @@
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 
-const Logo = ({className='mb-[100px]', titleClass="text-4xl md:text-6xl", descriptionClass="text-2xl md:text-4xl", logoSize=400}) => {
+const Logo = ({className='mb-[150px]', titleClass="text-4xl md:text-6xl", descriptionClass="text-2xl md:text-4xl", logoSize=400}) => {
     const t = useTranslations("hero");
     return (
         <div className={`relative flex flex-col items-center gap-2 md:gap-5 z-40 md:mb-0 ${className}`}>
