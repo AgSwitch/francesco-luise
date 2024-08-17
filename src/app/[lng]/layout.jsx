a04fbaedbdecf378180e3cab06ca177324f3bc9c
Momo M. <momomarcoj@gmail.com>
2024-08-17 21:26:03 +0200

    Fixed unstable language

--- src/app/[lng]/layout.jsx
+++ src/app/[lng]/layout.jsx
@@ -1,11 +1,10 @@
 import Navbar from '@/components/navbar/Navbar';
 import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
+import { getMessages, unstable_setRequestLocale } from 'next-intl/server';
 import { dir } from 'i18next';
 import '../globals.css';
 import { Lato } from 'next/font/google';
 import { availableLanguages } from '../../../global/languages';
-
 
 const lato = Lato({
     weight: ['100', '300', '400', '700', '900'],
@@ -17,7 +16,9 @@ export async function generateStaticParams() {
 }
 
 export default async function RootLayout({ children, params: { lng } }) {
-    const messages = await getMessages();
+    unstable_setRequestLocale(lng);
+
+    const messages = await getMessages(lng);
 
     return (
         <html lang={lng} dir={dir(lng)}>
