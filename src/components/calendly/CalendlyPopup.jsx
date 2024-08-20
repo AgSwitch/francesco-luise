a08ae0e498ba574721b5495509d517b57d8eb53b
Emanuele Costantini <costantiniemanuele96@gmail.com>
2024-08-21 00:38:02 +0200

    Fixed errors on build

--- src/components/calendly/CalendlyPopup.jsx
+++ src/components/calendly/CalendlyPopup.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useTranslations } from "next-intl";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PopupWidget } from "react-calendly";
 
 
@@ -8,12 +8,19 @@ import { PopupWidget } from "react-calendly";
 const CalendlyPopup = ({lng}) => {
 
     const t = useTranslations('contacts');
+    const [rootElement, setRootElement] = useState(null);
+
+    useEffect(() => {
+        setRootElement(document.body);
+    }, []);
+
+
 
   return (
     <div className="">
       <PopupWidget
         url={lng === 'it' ? "https://calendly.com/minimene/30min" : 'https://calendly.com/minimene/incontro-30-minuti-clone'}
-        rootElement={document.body}
+        rootElement={rootElement}
         text={t('calendly.title')}
         textColor="#ffffff"
         color="#0f172a"
