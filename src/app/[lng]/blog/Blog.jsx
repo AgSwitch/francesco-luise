ab29d781e399f5cbe9b84e1dbe6d1d8f459669b4
Momo M. <momomarcoj@gmail.com>
2024-09-15 18:25:50 +0200

    Fixed blog redirect if no it

--- src/app/[lng]/blog/Blog.jsx
+++ src/app/[lng]/blog/Blog.jsx
@@ -3,29 +3,29 @@ import BlogCard from "@/components/blogCard/BlogCard";
 import Loader from "@/components/loader/Loader";
 import useGetBlogPosts from "@/hooks/useGetBlogPosts";
 import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
 
-const Blog = ({lng}) => {
+const Blog = ({ lng }) => {
   const { lastBlogPosts, loading, error } = useGetBlogPosts();
   const t = useTranslations("blogPage");
-
-  if(loading) return <Loader />
+  const router = useRouter();
+  if (lng !== "it") router.replace(`/${lng}/`);
+  if (loading) return <Loader />;
 
   return (
     <main
       id="blog"
       className="relative z-10 min-h-screen bg-secondary flex flex-col items-center justify-center gap-10 py-32"
     >
-        <div className="flex justify-center flex-col items-center py-8 text-center px-8">
-          <h1 className="text-3xl md:text-6xl font-bold">{t("title")}</h1>
-          <h2 className="text-xl md:text-3xl font-semibold">
-            {t("subtitle")}
-          </h2>
-        </div>
-        <div>
-          {lastBlogPosts.map((post, index) => (
-            <BlogCard key={index} index={index} post={post} lng={lng}/>
-          ))}
-        </div>
+      <div className="flex justify-center flex-col items-center py-8 text-center px-8">
+        <h1 className="text-3xl md:text-6xl font-bold">{t("title")}</h1>
+        <h2 className="text-xl md:text-3xl font-semibold">{t("subtitle")}</h2>
+      </div>
+      <div>
+        {lastBlogPosts.map((post, index) => (
+          <BlogCard key={index} index={index} post={post} lng={lng} />
+        ))}
+      </div>
     </main>
   );
 };
--- src/app/[lng]/blog/[slug]/BlogPost.jsx
+++ src/app/[lng]/blog/[slug]/BlogPost.jsx
@@ -4,10 +4,13 @@
 import Loader from '@/components/loader/Loader';
 import Post from '@/components/post/Post';
 import useGetBlogPost from '@/hooks/useGetBlogPost';
+import { useRouter } from 'next/navigation';
 
 const BlogPost = ({params}) => {
     const { post, loading } = useGetBlogPost(params.slug);
-
+    const router = useRouter();
+    console.log(params);
+    if (params.lng !== "it") router.replace(`/${params.lng}/`);
     if(loading){
         return <Loader />
     }
