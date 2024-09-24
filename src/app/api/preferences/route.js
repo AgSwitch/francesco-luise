be5cc0362ce270028ade613bb83eb19da2499650
Emanuele Costantini <costantiniemanuele96@gmail.com>
2024-09-24 14:18:43 +0200

    created calendly config in db and new api's to get and update calendly isActive attribute

---
+++ src/app/api/preferences/route.js
@@ -0,0 +1,40 @@
+import db from "@/lib/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+    const { searchParams } = new URL(request.url);
+    const config = searchParams.get('config');
+    const docRef = doc(db, "configs", config);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        return NextResponse.json({ data: docSnap.data() }, { status: 200 });
+    } else if (!docSnap.exists) {
+        return NextResponse.json({ error: "No such document!" }, { status: 404 });
+    } else {
+        return NextResponse.json({ error: "Error getting document:" }, { status: 500 });
+    }
+}
+
+export async function PUT(request) {
+    const { searchParams } = new URL(request.url);
+    const isActive = searchParams.get('isActive');
+    const config = searchParams.get('config');
+
+    console.log(isActive, config);
+    const docRef = doc(db, "configs", config);
+
+    console.log(docRef);
+    try {
+        await updateDoc(docRef, {
+            isActive: isActive === 'true' ? true : false
+        });
+
+        return NextResponse.json({ message: "Cofig updated Successfully"  }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: "Error updating document:" }, { status: 500 });
+    }
+
+}
