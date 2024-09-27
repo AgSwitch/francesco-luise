7b767bc0257d89700b0fd5ae648d4ab380ccb27c
Momo M. <momomarcoj@gmail.com>
2024-09-27 22:32:10 +0200

    Update hero image

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,7 @@
-import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Logo from "../logo/Logo";
 
 const Hero = () => {
-  const t = useTranslations("hero");
   return (
     <section
       id="home"
@@ -15,11 +13,12 @@ const Hero = () => {
       <div className=" w-fit h-fit absolute bottom-0 right-0 flex xl:hidden">
         <Image
           draggable={false}
-          src={"/hero.png"}
+          src={"/hero.webp"}
           className="object-cover h-screen md:h-fit md:object-contain drop-shadow-4xl"
           alt="Francesco Luise"
           width={650}
           height={650}
+          quality={100}
           priority
         />
       </div>
@@ -28,10 +27,11 @@ const Hero = () => {
         <div className="relative w-full h-full">
         <Image
           draggable={false}
-          src={"/hero.png"}
+          src={"/hero.webp"}
           className="object-cover h-screen md:h-fit md:object-contain drop-shadow-4xl"
           alt="Francesco Luise"
           fill
+          quality={100}
           priority
           />
           </div>
